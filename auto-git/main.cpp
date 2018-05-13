ec9ded0eca4c3feeacbacadcca7ab68de568ed2d
cody-ray <crank.20@osu.edu>
2018-05-13 11:34:27 -0700

    Initial Commit

---
+++ auto-git/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  auto-git
+//
+//  Created by cody's macbook on 5/13/18.
+//  Copyright © 2018 crank. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
