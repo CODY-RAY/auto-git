bd727745b5c17c1323b7a9e30f380aa361d0c8e6
cody-ray <crank.20@osu.edu>
2018-05-14 18:24:03 -0700

    first

--- auto-git/main.cpp
+++ auto-git/main.cpp
@@ -2,14 +2,33 @@
 //  main.cpp
 //  auto-git
 //
-//  Created by cody's macbook on 5/13/18.
+//  Created by Ray's macbook on 5/13/18.
 //  Copyright © 2018 crank. All rights reserved.
 //
 
 #include <iostream>
+#include <boost/process.hpp>
+#include <boost/filesystem.hpp>
+#include <string>
 
+namespace bp = boost::process;
+namespace fs = boost::filesystem;
+
+using namespace std;
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    bp::opstream in;
+    bp::ipstream out;
+    fs::path _path = fs::current_path();
+    _path =  _path / ".git";
+    //main if
+    if (is_directory(_path) ){
+        bp::child c("crontab ", bp::std_out > out, bp::std_in < in);
+        in << "0,16,31,46 * * * * git -C "+ fs::current_path().string() +"pull"<< endl ;
+        c.terminate();
+    }else {
+        cout<<"fatal: Not a git repository: .git \n";
+    }
     return 0;
 }
+
