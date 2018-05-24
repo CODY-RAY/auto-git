871f10c7e8a0831669baa865adef5444eee078e4
cody-ray <crank.20@osu.edu>
2018-05-24 02:26:08 -0700

    added install.sh

---
+++ install.sh
@@ -0,0 +1,19 @@
+main() {
+  set -e
+  umask g-w,o-w
+  file=/usr/local/bin/auto-git
+  env curl -L -o "$file" https://github.com/CODY-RAY/auto-git/releases/download/v0.0.2/auto-git  || {
+    printf "Error: download failed\n"
+    exit 1
+  }
+  if [ -e "$file" ]; then
+     env chmod +x "$file"|| {
+    printf "Error: linking failed\n"
+    exit 1
+  }
+  else 
+    echo "File does not exist"
+  fi 
+}
+
+main
